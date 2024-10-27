ca50bb8f1b23e8293f2be8fc3ef16feddc972871
m27iu4 <mariusionita90@yahoo.com>
2024-10-27 12:46:30 +0200

    Initial commit

---
+++ FlightBooking/script.js
@@ -0,0 +1,25 @@
+const nameOnPass = "NameOfThePassanger";
+console.log("Passanger Name: " + nameOnPass);
+const nameOnId = "NameOnTheId";
+const nameMatch = nameOnPass == nameOnId;
+console.log("ID confirmed? " + nameMatch);
+const length = 5;
+const doesLengthPass = length < 30;
+const height = 10;
+const doesHeightPass = height < 55;
+const isHandBaggageEligible = doesLengthPass && doesHeightPass;
+console.log("Is Hand Baggage Eligible? " + isHandBaggageEligible);
+const flightDes = "Paris";
+const returnFrom = "Venice";
+const isRoundTrip = flightDes == returnFrom;
+console.log("Round Trip Check? " + isRoundTrip);
+const bookings = 10;
+const totalCapacity = 30;
+const isFlightAvailable = bookings <= totalCapacity;
+console.log("Are seats available? " + isFlightAvailable);
+
+document.querySelector("#name").innerHTML = nameOnPass;
+document.querySelector("#id").innerHTML = nameMatch;
+document.querySelector("#bag").innerHTML = isHandBaggageEligible;
+document.querySelector("#des").innerHTML = isRoundTrip;
+document.querySelector("#seat").innerHTML = isFlightAvailable;
